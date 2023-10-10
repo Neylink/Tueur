3a627b77729133585d429dece982ab2d0341e1b0
Neylink <146921880+Neylink@users.noreply.github.com>
2023-10-10 22:43:16 +0200

    Add files via upload

---
+++ exo tueur.js
@@ -0,0 +1,49 @@
+let names=['Charles', 'Henry', 'Michel', 'Jean', 'Martin'];
+names.push('Jason');
+
+names.forEach(name =>{
+    name += 'Sportif ';
+    console.log('Charles')
+});
+names.forEach(name =>{
+    name += 'Aventurier ';
+    console.log('Michel')
+});
+names.forEach(name =>{
+    name += 'Nerd ';
+    console.log('Henry')
+});
+names.forEach(name =>{
+    name += 'Blond';
+    console.log('Martin')
+});
+names.forEach(name =>{
+    name += 'Trouillard ';
+    console.log('Jean')
+});
+
+const character  = {
+    nom: "Jason", 
+    santé: 100,
+};
+console.log([names]);
+
+{
+    functionCombat
+if Personnage attaque Jason alors Jason = 0.3;
+else (valeurs aléatoire); 
+
+if Jason non tué alors survivant meurt;
+else survivant esquive et Jason santé = - 10; 
+
+if Jason non tué alors survivant meurt; 
+else Jason santé = -15;
+
+}
+
+forEach functionCombat affichés ('Jason a tué X'; 'X a esquivé et a infligé X dmg', 'Jason est mort'
+'Les survivants ont gagné mais RIP à X, X, X'); 
+
+
+
+
